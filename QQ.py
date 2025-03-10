d303811f51bbf48470297a36e7c66a1a6cebef78
ky10237 <ky1023767@gmail.com>
2025-03-10 16:07:18 +0900

    Update QQ.py

--- QQ.py
+++ QQ.py
@@ -3,3 +3,7 @@ print("지금",Pro_List[0],"을 공부합니다.\n"
       "학습도구는",Pro_List[1],"입니다\n"
       "그리고 버전은",Pro_List[2],"입니다");
 
+Pro_List=["Python","Pycharm",3.0]
+Pro_List.append("Programming")
+print("값이 추가된 리스트의 길이는",len(Pro_List),"입니다\n"
+      "담겨진 값들은 아래와 같습니다 \n",Pro_List);
