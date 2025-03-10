927de01bdc51829dcc85236a3cea8c410febb8ef
ky10237 <ky1023767@gmail.com>
2025-03-10 15:24:38 +0900

    Add files via upload

---
+++ yun1.py
@@ -0,0 +1,37 @@
+print("Hello python")
+print("안녕하세요. 배윤지입니다.")
+import os as A
+Folder_path=A.getcwd()
+print(Folder_path)
+
+
+A=1
+B="어렵지 않아요"
+print(A,B)
+
+A="python"
+print(A)
+
+C="What's your name?"
+print(C);
+
+C='''우리는 지금 파이썬을 학습하고 있습니다'''
+print(C)
+
+a=[3,"가",4];
+b=a[0]+6;
+print(b);
+
+Pro_Name="Python"
+Pro_Tool="Pycharm"
+print("지금",Pro_Name,"을 공부합니다. 학습도구는", Pro_Tool,"입니다")
+
+f="나",a
+print(f)
+
+print("지금'{0}'을 공부합니다.\학습 도구는 '{1}'입니다.",format((Pro_Name,Pro_Tool)))
+
+my_Name="배윤지"
+my_home="충남 천안시"
+my_age=26
+print("나의",my_Name,"이며, 나이는 ", my_age,"이고",my_home,"에 살고 있습니다.")
